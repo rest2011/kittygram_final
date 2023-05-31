326137b92cca63070fa0898693a7c9c786a40f56
rest2011 <renatig@yamdex.ru>
2023-05-31 19:46:33 +0500

    1 review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+
+ALLOWED_HOSTS = ['158.160.0.4', '127.0.0.1', 'localhost', 'restkit.sytes.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
